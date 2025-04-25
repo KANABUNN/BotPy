b98c038ce6d0f366ad0384fee3dbf715342f8d1d
KANABUN <kanabun.kenshi@gmail.com>
2025-04-25 14:19:36 +0900

    起動方法の選択．

--- index.py
+++ index.py
@@ -3,11 +3,18 @@ import os
 import numpy as np
 import json
 
+boot = input('起動方法を選択してください (test/main): ')
+if boot != 'test' and boot != 'main':
+    print('無効な起動方法です.')
+    exit()
+elif boot == 'test':
+    TOKEN = os.getenv('DCTOKEN_TEST')
+elif boot == 'main':
+    TOKEN = os.getenv('DCTOKEN')
 Role_list = np.array(['Main_Role' , 'Guest_Role' , 'Bot_Role' , 'Temp_Role'])
 config = json.load(open('config_t.json', 'r', encoding='utf-8'))
 for i in range(len(Role_list)):
     Role_list[i] = config[Role_list[i]]
-TOKEN = os.getenv('DCTOKEN2')
 intents = discord.Intents.none()
 intents.presences =True
 intents.members = True
@@ -23,4 +30,7 @@ async def on_member_join(member):
     print(f'{member.name} が参加しました.')
     await member.add_roles(Role_list[3])
 
-client.run(TOKEN)
+try:
+    client.run(TOKEN)
+except discord.errors.LoginFailure: 
+    print('無効なトークンです.')
