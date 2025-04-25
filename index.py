8ca84c1c33ff6763d2d3a0141f07b868aba5ca20
KANABUN <kanabun.kenshi@gmail.com>
2025-04-25 13:45:32 +0900

    jsonファイルから配列に値を設定．

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
 import discord
 import os
 import numpy as np
+import json
 
 Role_list = np.array(['Main_Role' , 'Guest_Role' , 'Bot_Role' , 'Temp_Role'])
+config = json.load(open('config_t.json', 'r', encoding='utf-8'))
+for i in range(len(Role_list)):
+    Role_list[i] = config[Role_list[i]]
 TOKEN = os.getenv('DCTOKEN2')
 intents = discord.Intents.none()
 intents.presences =True
@@ -17,10 +21,6 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     print(f'{member.name} が参加しました.')
-    await member.add_roles(Temp_Role)
+    await member.add_roles(Role_list[3])
 
-async def Road_Role():
-    Member_Role = client.guild.get_role()
-
-   
 client.run(TOKEN)
