84c256bc8c74d3b027d61bd5f0613384f3db3a86
KANABUN <kanabun.kenshi@gmail.com>
2025-07-09 12:58:43 +0900

    Alpha

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ui import Button, View
 import os
 import numpy as np
 import json
@@ -35,9 +36,25 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     print(f'{member.name} が参加しました.')
-    channel = member.guild.get_channel(int(config[Channel_list[0]]))
-    await channel.send(f'@everyone\n{member.display_name} さんが参加しました.')
-    await member.add_roles(member.guild.get_role(int(config[Role_list[3]])))
+    channel = member.guild.get_channel(int(config["User"][Channel_list[0]]))
+    await channel.send(f'@everyone\n{member.display_name} さんが参加しました.', view=ButtonView())
+    await member.add_roles(member.guild.get_role(int(config["User"][Role_list[3]])))
+
+# ボタンの設定
+class ButtonView(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.value = None
+
+    @discord.ui.button(label="メンバー権限を与える", style=discord.ButtonStyle.secondary)
+    async def mem_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message(f'{interaction.user.display_name}がメンバー権限を与えました.')
+        self.stop()
+
+    @discord.ui.button(label="ゲスト権限を与える", style=discord.ButtonStyle.secondary)
+    async def guest_button(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message(f'{interaction.user.display_name}がゲスト権限を与えました.')
+        self.stop()
 
 # 起動
 try:
